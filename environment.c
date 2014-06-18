3f664f692cfbe015a92726cfdd93fb93225542c1
diadara <nithin111@gmail.com>
2014-06-18 12:45:17 +0530

    LUA: add querry string parsing

--- environment.c
+++ environment.c
@@ -33,6 +33,87 @@ static int mk_lua_print(lua_State *L)
   return 0;
 }
 
+
+static int  mk_lua_querry_to_table(lua_State *L)
+{
+  
+  char *key;
+  char *value;
+  char *strtok_state;
+  int t;
+
+  lua_getglobal(L, "mk");
+  lua_getfield(L, -1, "request");
+  lua_getfield(L, -1, "query_string");
+  char *querry_s = lua_tostring(L,-1);
+  
+  lua_newtable(L);
+  lua_newtable(L);              /* [table, table] */
+  
+  
+  key = strtok_r(querry_s, "&", &strtok_state);
+  while (key) {
+    value = strchr(key, '=');
+    if (value) {
+      *value = '\0';      /* Split the string in two */
+      value++;            /* Skip passed the = */
+    }
+    else {
+      value = "1";
+    }
+    /*
+      store the key and values into tables
+    */
+
+    lua_getfield(L, -1, key);   /* [VALUE, table<s,t>, table<s,s>] */
+    /* borrowed from apache mod_lua */
+    t = lua_type(L, -1);
+    switch (t) {
+    case LUA_TNIL:
+    case LUA_TNONE:{
+            lua_pop(L, 1);      /* [table<s,t>, table<s,s>] */
+            lua_newtable(L);    /* [array, table<s,t>, table<s,s>] */
+            lua_pushnumber(L, 1);       /* [1, array, table<s,t>, table<s,s>] */
+            lua_pushstring(L, value);   /* [string, 1, array, table<s,t>, table<s,s>] */
+            lua_settable(L, -3);        /* [array, table<s,t>, table<s,s>]  */
+            lua_setfield(L, -2, key);   /* [table<s,t>, table<s,s>] */
+            break;
+        }
+    case LUA_TTABLE:{
+            /* [array, table<s,t>, table<s,s>] */
+            int size = lua_rawlen(L, -1);
+            lua_pushnumber(L, size + 1);        /* [#, array, table<s,t>, table<s,s>] */
+            lua_pushstring(L, value);   /* [string, #, array, table<s,t>, table<s,s>] */
+            lua_settable(L, -3);        /* [array, table<s,t>, table<s,s>] */
+            lua_setfield(L, -2, key);   /* [table<s,t>, table<s,s>] */
+            break;
+        }
+    }
+
+    /* L is [table<s,t>, table<s,s>] */
+    /* build simple */
+    lua_getfield(L, -2, key);   /* [VALUE, table<s,s>, table<s,t>] */
+    if (lua_isnoneornil(L, -1)) {       /* only set if not already set */
+        lua_pop(L, 1);          /* [table<s,s>, table<s,t>]] */
+        lua_pushstring(L, value);       /* [string, table<s,s>, table<s,t>] */
+        lua_setfield(L, -3, key);       /* [table<s,s>, table<s,t>]  */
+    }
+    else {
+        lua_pop(L, 1);
+    }
+
+
+
+
+
+    key = strtok_r(NULL, "&", &strtok_state);
+  }    
+
+  return 2;
+}
+
+
+
 void mk_lua_init_env_response(lua_State *L, struct session_request *sr)
 {
   UNUSED_VARIABLE(sr);
@@ -157,8 +238,8 @@ void mk_lua_init_env_request(lua_State *L,
 
   __lua_pushmkptr(L, sr->query_string);
   lua_setfield(L, -2, "query_string");
-  /* TODO parse query string and POST data into arrays */
-
+  /* TODO parse POST data into arrays */
+    
   __lua_pushmkptr(L, sr->data);
   lua_setfield(L, -2, "data");
       
@@ -189,7 +270,8 @@ void mk_lua_init_env_request(lua_State *L,
     lua_settable(L,-3);
   }
 
-    
+  lua_pushcfunction(L, mk_lua_querry_to_table);
+  lua_setfield(L, -2, "parseargs");
 
  error:
   if (tmpuri) mk_api->mem_free(tmpuri);
